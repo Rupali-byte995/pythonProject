f0fbe9f886c0dd9840bb1f04b00289ddc699ccfd
Rupali-byte995 <rupalib93@gmail.com>
2023-10-21 15:18:15 +0530

    My latest commit

--- pythonProject/16oct/task00316oct.py
+++ pythonProject/16oct/task00316oct.py
@@ -8,6 +8,12 @@ side 1, side 2 and side 3
 output - Eq, Iso, Scalene -
 Eq. = side 1 == side 2 = side 3
 """
-side1=int(input("Enter side 1"))
-side2=int(input("Enter side 2"))
-side3=int(input("Enter side 3"))
+side1 = int(input("Enter side 1"))
+side2 = int(input("Enter side 2"))
+side3 = int(input("Enter side 3"))
+if (side1 == side2 and side2 == side3 and side3 == side1):
+    print("Equalateral triangle")
+elif (side1 == side2 or side2 == side3 or side3 == side1):
+    print("Isosceles")
+else:
+    print("Scalene")
--- pythonProject/16oct/task216oct.py
+++ pythonProject/16oct/task216oct.py
@@ -6,7 +6,7 @@ Input = 2024
 Output = Leap year
 """
 year = int(input("Enter year to check if it is leap year or not"))
-if (year % 4 == 0 or(year%400 == 0 and year%100!=0)):
+if year % 4 == 0 or year%400 == 0 and year%100!=0:
     print("It is a leap year")
 else:
     print("Not a leap year")
